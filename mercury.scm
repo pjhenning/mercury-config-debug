14efe0c5fde31407d025fa60023cdc454d312d00
Patrick J. Henning <path@fea.st>
2019-12-03 20:03:04 -0800

    .

--- mercury.scm
+++ mercury.scm
@@ -36,24 +36,8 @@
         (modify-phases %standard-phases
            (add-after 'unpack 'fix-hardcoded-paths
              (lambda _
-               (let 
-                  (
-                    (hcp_files (list 
-                                   "bindist/bindist.Makefile"
-                                   "bindist/bindist.Makefile.in"
-                                   "tests/benchmarks/Makefile.mercury"
-                                   "scripts/Mmake.vars.in"
-                                   "boehm_gc/PCR-Makefile"
-                                   "boehm_gc/Makefile.dj"
-                                   "boehm_gc/Makefile.direct"
-                                   "boehm_gc/autogen.sh"
-                    ))
-                  )
-                  (
-                    (write "wow")
-                    (write hcp_files)
-                  )
-                )
+               (substitute* "configure"
+                  (("export SHELL") (string-append "export CONFIG_SHELL=" (which "sh") "\nexport SHELL=" (which "sh"))))
              #t)
             )
         )
